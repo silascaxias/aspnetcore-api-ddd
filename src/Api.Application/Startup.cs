612ba75567bcd4c2601e843961542a8f3ab9d15f
Silas S. Caxias <silascaxias@alunos.fho.edu.br>
2020-01-26 15:06:18 -0300

    Implementando Swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,6 +1,8 @@
+using System;
 using Api.CrossCutting.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +23,20 @@ namespace Application
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
+            services.AddSwaggerGen( c => {
+                c.SwaggerDoc ("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo{
+                        Title = "AspNetCore 3.1",
+                            Version = "v1",
+                            Description = "Projeto simples de web API em ASP.NET Core aplicando conceitos da estrutura DDD.",
+                            Contact = new Microsoft.OpenApi.Models.OpenApiContact {
+                                Name = "Silas da Silva Caxias",
+                                Url =  new Uri("https://github.com/silascaxias/aspnetcore-api-ddd")
+                            }
+                    }
+                );
+            });
+
             services.AddControllers();
         }
 
@@ -31,6 +47,18 @@ namespace Application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Ativando middlewares para uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Projeto em AspNetCore 3.1");
+            });
+
+            // Redireciona o Link para o Swagger, quando acessar a rota principal
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
 
             app.UseRouting();
 
