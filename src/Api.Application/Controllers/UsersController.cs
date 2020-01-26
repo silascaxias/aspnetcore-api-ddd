7d6f9eae69481e996e67c80f9ab3da747ce7d9ab
Silas S. Caxias <silascaxias@alunos.fho.edu.br>
2020-01-26 02:19:24 -0300

    Criado Users Controller: Get, GetById, Post

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,6 +25,40 @@ namespace Api.Application.Controllers
                 return Ok(await service.GetAll());
             }
             catch (ArgumentException e)
+            {                
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetById")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            try
+            {
+                return Ok(await service.Get(id));
+            }
+            catch (ArgumentException e)
+            {                
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UserEntity user)
+        {
+            try
+            {
+                var result = await service.Post(user);
+                if (result != null) {
+                    return Ok(result);
+                }
+                else 
+                {
+                    return BadRequest();
+                }
+            }
+             catch (ArgumentException e)
             {                
                 return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
             }
